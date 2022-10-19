692be0befeabbd4d684e7608bd63b994d91fe70a
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-10-18 21:22:46 -0400

    Imported new animations
    Added sprite animations to project.

--- Thesis AOE/Assets/Scripts/PlayerController2D.cs
+++ Thesis AOE/Assets/Scripts/PlayerController2D.cs
@@ -4,27 +4,30 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
-    public float moveSpeed = 5f;
-    public Rigidbody2D rb2d;
-    Vector2 movement;
+    public float speed;
+    private Rigidbody2D rb2d;
+    private Vector3 posChange;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        // This code is responsible for which way the player is moving
-        movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
+        posChange = Vector3.zero;
+        posChange.x = Input.GetAxisRaw("Horizontal");
+        posChange.y = Input.GetAxis("Vertical");
+        if (posChange != Vector3.zero)
+        {
+            MoveCharacter();
+        }
     }
 
-    // This code will help make the player movement more smooth
-    void FixedUpdate()
+    void MoveCharacter()
     {
-        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb2d.MovePosition(transform.position + posChange * speed * Time.deltaTime);
     }
 }
