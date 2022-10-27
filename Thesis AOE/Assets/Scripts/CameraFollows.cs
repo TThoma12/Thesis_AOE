38a09f559d3f1ec4d89ef9dc760b66beda580876
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-10-27 18:07:04 -0400

    Created new script
    Made new script, and named it CameraFollows.

---
+++ Thesis AOE/Assets/Scripts/CameraFollows.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollows : MonoBehaviour
+{
+    public Transform target;
+    public float smoothing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void LateUpdate()
+    {
+        if (transform.position != target.position)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position, smoothing);
+        }
+    }
+}
