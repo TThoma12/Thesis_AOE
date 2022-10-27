ca00b77b7b8831737303ed10d60afaa73e89561d
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-10-27 16:09:11 -0400

    Edited Player controller script
    Added new components and variables to PlayerController2D script.

--- Thesis AOE/Assets/Scripts/PlayerController2D.cs
+++ Thesis AOE/Assets/Scripts/PlayerController2D.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class PlayerController2D : MonoBehaviour
 {
+    public float moveSpeed = 5f;
+    public Rigidbody2D rb2d;
+    Vector2 movement;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,12 @@ public class PlayerController2D : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+        rb2d.MovePosition(rb2d.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
 }
