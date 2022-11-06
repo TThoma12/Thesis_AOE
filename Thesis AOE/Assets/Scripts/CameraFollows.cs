5814b281a5790e694e58a36e8adfd31742769bb2
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-11-06 11:20:01 -0500

    Edited Camera Follows script
    Implemented camera bounds in Camera Follows script.

--- Thesis AOE/Assets/Scripts/CameraFollows.cs
+++ Thesis AOE/Assets/Scripts/CameraFollows.cs
@@ -6,6 +6,8 @@ public class CameraFollows : MonoBehaviour
 {
     public Transform target;
     public float smoothing;
+    public Vector2 maxPos;
+    public Vector2 minPos;
 
     // Start is called before the first frame update
     void Start()
@@ -24,6 +26,12 @@ public class CameraFollows : MonoBehaviour
         if (transform.position != target.position)
         {
             Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+
+            // This will keep the camera in bounds for the player
+            targetPosition.x = Mathf.Clamp(targetPosition.x, minPos.x, maxPos.x);
+            targetPosition.y = Mathf.Clamp(targetPosition.y, minPos.y, maxPos.y);
+
+            // This will allow smooth movement for the camera when it's following the target position
             transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
         }
     }
