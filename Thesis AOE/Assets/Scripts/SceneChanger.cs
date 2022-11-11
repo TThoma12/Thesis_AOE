42228bccde743c104e97a07d6ded84051afda610
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-11-11 16:58:46 -0500

    Made updates in project scene
    Attached SceneChanger script to empty game object.

--- Thesis AOE/Assets/Scripts/SceneChanger.cs
+++ Thesis AOE/Assets/Scripts/SceneChanger.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneChanger : MonoBehaviour
 {
+    public string sceneToLoad;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,13 @@ public class SceneChanger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !other.isTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
+        }
     }
 }
