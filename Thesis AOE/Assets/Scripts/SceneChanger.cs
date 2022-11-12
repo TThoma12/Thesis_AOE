a821ca1ac796a62297e036d8da8578ee49ab93f6
TThoma12 <tthoma12@student.fitchburgstate.edu>
2022-11-12 17:06:32 -0500

    Edited script
    Made changes in Scene Changer script.

--- Thesis AOE/Assets/Scripts/SceneChanger.cs
+++ Thesis AOE/Assets/Scripts/SceneChanger.cs
@@ -8,6 +8,7 @@ public class SceneChanger : MonoBehaviour
     public string sceneToLoad;
     public Vector2 playerPosition;
     public VectorValue playerStorage;
+    public GameObject transitionPanel;
 
     // Start is called before the first frame update
     void Start()
@@ -19,6 +20,16 @@ public class SceneChanger : MonoBehaviour
     void Update()
     {
         
+    }
+
+    // This will control the transitions between scenes for the player
+    private void Awake()
+    {
+        if (transitionPanel != null)
+        {
+            GameObject panel = Instantiate(transitionPanel, Vector3.zero, Quaternion.identity) as GameObject;
+            Destroy(panel, 1);
+        }
     }
 
     public void OnTriggerEnter2D(Collider2D other)
